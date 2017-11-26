6c6dd0aec71495a5c01b99f309cfddaf8e58d9c5
Rayad-Lackhan <32082203+Rayad-Lackhan@users.noreply.github.com>
2017-11-25 23:34:42 -0400

    change of header
    asdsa

--- templates/header.php
+++ templates/header.php
@@ -1,58 +1,44 @@
 <!DOCTYPE html>
-
 <html>
+<head>
+  <title>Profile</title>
+  <link href="../public/css/bootstrap.css" rel="stylesheet"/>
+  <link href="../public/css/styles.css" rel="stylesheet"/>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
-    <head>
 
-        <link href="../public/css/bootstrap.css" rel="stylesheet"/>
-        <link href="../public/css/styles.css" rel="stylesheet"/>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <?php if (isset($title)): ?>
+        <title>FARMLAND: <?= htmlspecialchars($title) ?></title>
+  <?php else: ?>
+        <title>FARMLAND</title>
+  <?php endif ?>
 
-        <?php if (isset($title)): ?>
-            <title>Farmland<?= htmlspecialchars($title) ?></title>
-        <?php else: ?>
-            <title>Farmland</title>
-        <?php endif ?>
+  <script src="../public/js/jquery-1.10.2.min.js"></script>
+  <script src="../public/js/bootstrap.min.js"></script>
+  <script src="../public/js/scripts.js"></script>
+</head>
 
-        <script src="../public/js/jquery-1.10.2.min.js"></script>
-        <script src="../public/js/bootstrap.min.js"></script>
-        <script src="../public/js/scripts.js"></script>
+<body>
 
-    </head>
+  <nav class="navbar navbar-default navbar-fixed-top">
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <a class="navbar-brand" href="#"><span class="glyphicon glyphicon-tree-deciduous"></span> FARMLAND </a><br>
+      </div>
+      <ul class="nav navbar-nav">
+        <li><a href="#"><span class="glyphicon glyphicon-home"></span> HOME </a></li>
+        <li><a href="#"><span class="glyphicon glyphicon-shopping-cart"></span> SHOP </a></li>
+        <li><a href="#"><span class="glyphicon glyphicon-info-sign"></span> HELP </a></li>
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+          <li><a href="#"><span class="glyphicon glyphicon-user"></span> PROFILE </a></li>
+          <li><a href="#"><span class="glyphicon glyphicon-log-out"></span> Log Out </a></li>
+      </ul>
+    </div>
+  </nav>
 
-    <body>
 
-        <div class="container">
 
-            <div class="row">
-                <a href="../public/index.php"><img alt="C$50 Finance" src="../public/img/logo.png"/></a>
-            </div>  
 
-            <nav class="navbar navbar-default">
-              <div >
-                <div class="navbar-header">                  
-                  <a class="navbar-brand" href="../public/index.php"><span class="glyphicon glyphicon-home"></span></a>
-                </div>
-
-                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                  <ul class="nav navbar-nav">
-                    <li><a href="../public/quote.php">Quote</a></li>
-                    <li><a href="../public/buy.php">Buy</a></li>
-                    <li><a href="../public/history.php">History</a></li>
-                    
-                  </ul>
-                  
-                  <ul class="nav navbar-nav navbar-right">
-                    <?php if (empty($_SESSION["id"])): ?>
-                        <li><a href="../public/login.php">Login</a></li>
-                        <li><a href="../public/register.php">Register</a></li>
-                    <?php else: ?>
-                        <li><a href="../public/logout.php">Logout</a></li> 
-                    <?php endif; ?>       
-                  </ul>
-                </div>
-              </div>
-        </nav>
-    </div>    
-
-    <div class="row">
+</body>
+</html>
---
+++ templates/home.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Homepage</title>
+  <link href="../public/css/bootstrap.css" rel="stylesheet"/>
+  <link href="../public/css/styles.css" rel="stylesheet"/>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <script src="../public/js/jquery-1.10.2.min.js"></script>
+  <script src="../public/js/bootstrap.min.js"></script>
+  <script src="../public/js/scripts.js"></script>
+  <?php
+    include("header.php");
+   ?>
+
+   <style>
+      h1{
+
+        color:blue;
+      }
+   </style>
+
+</head>
+<body>
+  <div class="container">
+    <h1> Welcome to the CS Farmland Website! </h1>
+  </div>
+
+
+</body>
+</html>
