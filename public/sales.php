5f17fed1abae9c28aa8ed03192b4843bae27c2eb
Daryl <currygod@hotmail.com>
2017-12-02 22:44:35 -0400

    Updated queries

--- public/sales.php
+++ public/sales.php
@@ -18,9 +18,8 @@
 
 				 $items = query("SELECT name FROM products WHERE product_id = ?", $row["product_id"]);
 				 $b = $items[0]["name"];
-				 $items = query("SELECT price, user_id FROM products_for_sale WHERE product_id = ?", $row["product_id"]);
-				 $c = $items[0]["price"];
-				 $items = query("SELECT username FROM users WHERE user_id = ?", $items[0]["user_id"]);
+				 $c = $row["total"];
+				 $items = query("SELECT username FROM users WHERE user_id = ?", $row["buyer_id"]);
 				 $d = $items[0]["username"];
 
 				
@@ -31,7 +30,7 @@
 				 "date" => $row["date_sold"],
 				 "units" => $row["amount"],
 				 "product" => $b,
-				 "total" => $c * $row["amount"],
+				 "total" => $c, 
 				 "client" => $d,
 				 
 				 ];
