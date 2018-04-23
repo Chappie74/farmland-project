50b10b966459593a13da161c3e40c11a9c7e0ead
Daryl <currygod@hotmail.com>
2018-04-23 13:57:37 -0400

    sell functionality atempt

--- public/sell.php
+++ public/sell.php
@@ -1,22 +1,20 @@
 <?php 
 
 // configuration
-    require("../includes/config.php"); 
+    require("../includes/config.php");
+    require("../includes/classes.php");
     $css_file = "../public/css/sell_t.css";
     $script_file = "../public/js/sell_t.js";
 
     if($_SERVER["REQUEST_METHOD"] == "POST")
     {
 
-    	$img = $_FILES["product_image"];
-        $product_name = trim(strtolower($_POST["product_name"])); 
-        $category = $_POST["category"];
-        $amount = floor($_POST["amount"]);
-        $price = abs($_POST["price"]);
+        data = new Database();
+
 
                 //file upload variables      
     	$fileName = $_FILES["product_image"]["name"];
-    	$fileTmpName = $_FILES["product_image"]["tmp_name"];
+    	$fileTmpName = $_FILES"product_image"]["tmp_name"];
     	$fileError = $_FILES["product_image"]["error"];
         $accepted_size = 1049600;
     	$fileSize = $_FILES["product_image"]["size"];
@@ -37,16 +35,34 @@
 
                     $sql = "SELECT category_id FROM categories WHERE name = ? LIMIT 1";
                     $rows = query($sql,$category);//query the category id
-                    
-                    $sql = "INSERT INTO products (name,category_id, image) VALUES (?,?,?);";
+                   
                     $results = query($sql,$product_name,$rows[0]["category_id"],$fileDestination); //insert product information
+
+                    $insert_array1 = array(
+                        'name' =>$product_name,
+                        'category_id' => $rows[0]["category_id"],
+                        'image' => $fileDestination,
+                    );
+                          
+                    $sql = "INSERT INTO products (name,category_id, image) VALUES (?,?,?);";
+                    data->query($sql,$insert_array1);
 
                     
                     $last_product_id = query("SELECT LAST_INSERT_ID() AS id");
-                    
 
-                    $sql = "INSERT INTO products_for_sale (user_id,product_id,amount,date_listed, price) VALUES (?,?,?,CURRENT_DATE(),?)";
-                    $results = query($sql, $_SESSION["id"], $last_product_id[0]["id"],$amount,$price);    
+
+                     $insert_array2 = array(
+                        'user_id' =>$_SESSION["id"], 
+                        'product_id' => $last_product_id[0]["id"],
+                        'amount' => $amount,
+                        'date_listed' => CURRENT_DATE(),
+                        'price' =>$price, 
+                    );
+                    
+                    $sql = "INSERT INTO products_for_sale (user_id,product_id,amount,date_listed, price) VALUES (?,?,?,?,?)";
+                    data->query($sql,$insert_array2);
+                    
+                       
                     redirect("index.php");               
                     
                 }else   
@@ -58,10 +74,12 @@
     }
 
     $sql = "SELECT * FROM categories ORDER BY name;";
-    $rows = query($sql);
+    rows = new Database();
+    row->query($sql);
+
     // dump($rows);
     render("../templates/sell_t.php", ["title" => "Sell a product",
-    									"categories" => $rows,
+    									"categories" => rows,
     									"css" => $css_file,
     									"script" => $script_file
     								]);
